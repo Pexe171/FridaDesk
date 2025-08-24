90f4877ec338f14616bb75e6b5828af9b504daad
Pexe <davidhenriqusms18@gmail.com>
2025-08-24 12:28:01 -0400

    Delete build directory

--- build/publish-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Autor: Pexe - Instagram: David.devloli
-set -e
-RID=linux-x64
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dotnet publish "$SCRIPT_DIR/../src/FridaHub.App/FridaHub.App.csproj" -c Release -r $RID -p:PublishSingleFile=true -p:SelfContained=false -o "$SCRIPT_DIR/publish/$RID"
--- build/publish-win.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [string]$RID = "win-x64"
-)
-# Autor: Pexe - Instagram: David.devloli
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$OutputDir = Join-Path $ScriptDir "publish/$RID"
-dotnet publish "$ScriptDir/../src/FridaHub.App/FridaHub.App.csproj" -c Release -r $RID -p:PublishSingleFile=true -p:SelfContained=false -o $OutputDir
